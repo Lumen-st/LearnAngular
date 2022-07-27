e923cebe21ca13241923234c711a836b734778d2
adilsonfuta <adilson.futa@jupiter.co.ao>
2022-07-27 16:19:11 +0100

    ajustes de teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { MenuHomeComponent } from './menu-home/menu-home.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule  } from "@angular/forms";
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuHomeComponent,
-    HeroesComponent 
+    HeroesComponent,
+    HeroDetailComponent 
   ],
   imports: [
     BrowserModule,
