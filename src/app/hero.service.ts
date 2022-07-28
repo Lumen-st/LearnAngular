b3291a252df74e719e66ef847b386955e9774556
adilsonfuta <adilson.futa@jupiter.co.ao>
2022-07-28 15:28:39 +0100

    ajustando teste

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from "rxjs";
-import { Hero } from './hero';~
-import {HEROES} from './mock-heroes';
+import { Hero } from './hero';
+import * as heros from './mock-heroes';
 import {MessageService} from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
+  
   constructor(private messageService: MessageService) { }
 
   getHeroes():Observable<Hero[]>{
-    const heroes of(HEROES);
+    const heroes = of(heros.HEROES);
     this.messageService.add('HeroService:fetched heroes');
     return heroes;
   }
 }
+
+// import {HEROES} from './mock-heroes';
+// heroes = hero.HEROES;
